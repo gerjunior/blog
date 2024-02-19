33cb3253a49ed9cce0e6d32807a22720272fc275
Geraldo Silva <geraldojuni@hotmail.com>
2024-02-19 00:00:54 -0300

    feat: website header

---
+++ src/app/common/header.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function Header() {
+  return (
+    <nav className='flex w-full p-5 items-center bg-white shadow-sm text-sm sm:text-base'>
+      <Link
+        href='/'
+        className='min-w-max font-black'
+      >{`Geraldo's Dev Blog`}</Link>
+      <div className=' w-full flex gap-3 justify-end items-center font-semibold'>
+        <Link href='/blog'>Blog</Link>
+        <Link href='/about'>About</Link>
+        <button className='text-white bg-gradient-to-r from-[#764DFF] to-[#FFC3DF] rounded-lg p-3'>
+          Contact
+        </button>
+      </div>
+    </nav>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      Hello, Website!
+      <h1>{"Geraldo's Dev Blog"}</h1>
     </main>
   );
 }
