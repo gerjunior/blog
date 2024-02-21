bf700a4b67715e0e8ea8a97baff68a7e7fcb0603
Geraldo Silva <geraldojuni@hotmail.com>
2024-02-21 10:56:34 -0300

    fix: gap on footer

--- src/app/common/footer.tsx
+++ src/app/common/footer.tsx
@@ -4,7 +4,7 @@ import { Social } from './social';
 export function Footer() {
   return (
     <div className='bg-black flex flex-col items-center gap-10 p-16 text-white'>
-      <div className='w-full flex flex-col sm:flex-row gap-15 sm:gap-28 justify-center items-center font-semibold'>
+      <div className='w-full flex flex-col sm:flex-row gap-8 sm:gap-28 justify-center items-center font-semibold'>
         <Link href='/blog' className='hover:text-slate-400'>
           Blog
         </Link>
