807d012151fa221dd3a05523e839eb1052fd0bf7
Geraldo Silva <geraldojuni@hotmail.com>
2024-02-20 23:18:45 -0300

    feat: add hover + transitions to links

--- src/app/common/header.tsx
+++ src/app/common/header.tsx
@@ -3,14 +3,18 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <nav className='flex w-full p-5 items-center bg-white shadow-sm text-sm sm:text-base'>
+    <nav className='flex w-full p-5 pb-6 items-center bg-white shadow-sm text-sm sm:text-base'>
       <Link
         href='/'
-        className='min-w-max font-black sm:text-lg'
+        className='min-w-max font-black sm:text-xl md:text-2xl hover:text-slate-600'
       >{`Geraldo's Dev Blog`}</Link>
       <div className=' w-full flex gap-3 justify-end items-center font-semibold'>
-        <Link href='/blog'>Blog</Link>
-        <Link href='/about'>About</Link>
+        <Link href='/blog' className='hover:text-slate-600'>
+          Blog
+        </Link>
+        <Link href='/about' className='hover:text-slate-600'>
+          About
+        </Link>
         <button className='text-white bg-gradient-to-r from-[#764DFF] to-[#FFC3DF] rounded-lg p-3'>
           Contact
         </button>
