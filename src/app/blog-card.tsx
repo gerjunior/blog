e302477af1d2c98a4c99257aada82ba0d33b99ed
Geraldo Silva <geraldojuni@hotmail.com>
2024-03-01 09:46:03 -0300

    fix: adjust responsivity of the main page

--- src/app/blog-card.tsx
+++ src/app/blog-card.tsx
@@ -18,7 +18,7 @@ export function BlogCard() {
     <Link href='/blog/how-can-designers-prepare-for-the-future'>
       <div
         ref={wrapperDivRef}
-        className={`cursor-pointer w-80 xl:w-[56rem] sm:w-[36rem] md:w-[44rem] bg-white p-5 py-14 rounded-3xl flex flex-row justify-between shadow-md ${wrapperDivEffectClassSet}`}
+        className={`cursor-pointer w-80 xl:w-[56rem] sm:w-[36rem] md:w-[44rem] bg-white p-5 py-5 rounded-3xl flex flex-row justify-between shadow-md ${wrapperDivEffectClassSet}`}
       >
         <div className='ml-4 sm:ml-8 sm:w-1/2 md:mr-5 flex flex-col justify-center gap-4 items-start w-full'>
           <CategoryTag category='DESIGN' />
@@ -33,7 +33,7 @@ export function BlogCard() {
             width={450}
             height={450}
             alt='Eat, Sleep, Code, Repeat'
-            className='rounded-3xl'
+            className='rounded-3xl object-cover md:min-w-80 md:max-w-80 md:min-h-52 md:max-h-52 lg:min-h-52 lg:max-h-52 xl:min-w-[26rem] xl:max-w-[26rem] xl:min-h-64 xl:max-h-64'
             priority
           />
         </div>
